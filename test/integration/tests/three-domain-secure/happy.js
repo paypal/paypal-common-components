3d54b0380a55a8358ca98458b8280fdff3b0705d
Steven Mask <mnicpt@gmail.com>
2021-06-30 14:27:41 -0400

    Handle Inline Guest Failure Cases (#27)
    * fix onSuccess
    
    * test script
    
    * clear cache
    
    * update test
    
    * checks
    
    * test
    
    * webpack update
    
    * revert
    
    * remove line
    
    Co-authored-by: Steve Mask <smask@paypal.com>

--- test/integration/tests/three-domain-secure/happy.js
+++ test/integration/tests/three-domain-secure/happy.js
@@ -18,4 +18,21 @@ describe(`paypal 3ds component happy path`, () => {
             }).render('body');
         });
     });
+
+    it('should not render the 3ds component', () => {
+        return wrapPromise(({ expect, avoid }) => {
+            const nonce = '12345';
+            window.contingencyResult = {
+                success: false
+            };
+            return window.paypal.ThreeDomainSecure({
+                createOrder: () => 'XXXXXXXXXXXXXXXXX',
+                onSuccess:   avoid('onSuccess'),
+                onCancel:    avoid('onCancel'),
+                onError:     expect('onError'),
+                nonce
+
+            }).render('body');
+        });
+    });
 });
