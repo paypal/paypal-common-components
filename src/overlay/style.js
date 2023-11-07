e23fb029be5660316997169e238f3164d78e3196
Nate Bierdeman <nbierdeman@paypal.com>
2023-11-07 12:09:40 -0500

    Venmo overlay (#84)
    * Create Venmo overlay component
    
    * Add Venmo overlay styles
    
    * Add interrogativeMessage
    
    * Update styles
    
    * Update styles
    
    * Add ability to hide the close button
    
    * Add tests
    
    * Fix Flow
    
    * Fix test

--- src/overlay/style.js
+++ src/overlay/style.js
@@ -372,3 +372,357 @@ export function getContainerStyle({ uid }: {| uid: string |}): string {
         }
     `;
 }
+
+export function getVenmoSandboxStyle({ uid }: {| uid: string |}): string {
+  return `
+        #${uid}.venmo-checkout-sandbox {
+            display: block;
+            position: fixed;
+            top: 0;
+            left: 0;
+
+            width: 100%;
+            height: 100%;
+            width: 100vw;
+            height: 100vh;
+            max-width: 100%;
+            max-height: 100%;
+            min-width: 100%;
+            min-height: 100%;
+
+            z-index: 2147483647;
+
+            animation-duration: 0.3s;
+            animation-iteration-count: 1;
+            animation-fill-mode: forwards !important;
+            opacity: 0;
+        }
+
+        #${uid}.venmo-checkout-sandbox .venmo-checkout-sandbox-iframe {
+            display: block;
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+        }
+
+        #${uid}.venmo-checkout-sandbox .venmo-checkout-sandbox-iframe-full {
+            border: 0;
+            height: 100%;
+            width: 100vw;
+        }
+
+        @keyframes show-container {
+            from {
+                opacity: 0;
+            }
+
+            to {
+                opacity: 1;
+            }
+        }
+
+        @keyframes hide-container {
+            from {
+                opacity: 1;
+            }
+
+            50% {
+                opacity: 1;
+            }
+
+            to {
+                opacity: 0;
+            }
+        }
+    `;
+}
+
+export function getVenmoContainerStyle({ uid }: {| uid: string |}): string {
+  return `
+        #${uid} {
+            position: absolute;
+            z-index: 2147483647;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+
+            transform: translate3d(0, 0, 0);
+
+            background-color: black;
+            background-color: rgba(0, 0, 0, 0.4);
+            background: radial-gradient(50% 50%, ellipse closest-corner, rgba(0,0,0,0.6) 1%, rgba(0,0,0,0.8) 100%);
+
+            color: #fff;
+        }
+
+        #${uid} a {
+            color: #fff;
+        }
+
+        #${uid}.venmo-overlay-context-${CONTEXT.POPUP} {
+            cursor: pointer;
+        }
+
+        #${uid} a {
+            text-decoration: none;
+        }
+
+        #${uid} .venmo-checkout-modal {
+            box-sizing: border-box;
+            max-width: 400px;
+            min-width: 300px;
+            max-height: 100%;
+            top: 50%;
+            left: 50%;
+            position: absolute;
+            transform: translateX(-50%) translateY(-50%);
+            cursor: pointer;
+            text-align: center;
+        }
+
+        #${uid} .venmo-checkout-modal .venmo-interrogative-message {
+            font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+            font-style: normal;
+            font-size: 24px;
+            line-height: 32px;
+            text-align: center;
+            color: #FFFFFF;
+            margin-top: 32px;
+        }
+
+        #${uid}.venmo-overlay-loading .venmo-checkout-message, #${uid}.venmo-overlay-loading .venmo-checkout-continue {
+            display: none;
+        }
+
+        .venmo-checkout-loader {
+            display: none;
+        }
+
+        #${uid}.venmo-overlay-loading .venmo-checkout-loader {
+            display: block;
+        }
+
+        #${uid} .venmo-checkout-modal .venmo-checkout-logo {
+            cursor: pointer;
+        }
+
+        #${uid} .venmo-checkout-modal .venmo-checkout-logo img {
+            height: 46px;
+        }
+
+        #${uid} .venmo-checkout-modal .venmo-checkout-message {
+            font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+            font-style: normal;
+            font-weight: 400;
+            font-size: 16px;
+            line-height: 20px;
+            text-align: center;
+            color: #FFFFFF;
+            margin-top: 16px;
+        }
+
+        #${uid}.venmo-overlay-context-${CONTEXT.IFRAME} .venmo-checkout-message, #${uid}.venmo-overlay-context-${CONTEXT.IFRAME} .venmo-checkout-continue {
+            display: none;
+        }
+
+        #${uid} .venmo-checkout-modal .venmo-checkout-continue {
+            background: #0074DE;
+            border-radius: 24px;
+            border: none;
+            font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+            font-style: normal;
+            font-weight: 700;
+            font-size: 18px;
+            color: #FFFFFF;
+            margin-top: 44px;
+        }
+
+        #${uid} .venmo-checkout-modal .venmo-checkout-continue a {
+            line-height: 50px;
+        }
+
+        #${uid} .venmo-checkout-close {
+            font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+            font-style: normal;
+            font-weight: 700;
+            font-size: 18px;
+            line-height: 24px;
+            text-align: center;
+            background-color: transparent;
+            border: none;
+            color: #FFFFFF;
+            margin-top: 28px;
+        }
+
+        #${uid}.venmo-overlay-loading .venmo-checkout-close {
+            display: none;
+        }
+
+        #${uid} .venmo-checkout-iframe-container {
+            display: none;
+        }
+
+        #${uid}.venmo-overlay-context-${CONTEXT.IFRAME} .venmo-checkout-iframe-container,
+        #${uid}.venmo-overlay-context-${CONTEXT.IFRAME} .venmo-checkout-iframe-container > .${CLASS.OUTLET},
+        #${uid}.venmo-overlay-context-${CONTEXT.IFRAME} .venmo-checkout-iframe-container > .${CLASS.OUTLET} > iframe {
+            max-height: 95vh;
+            max-width: 95vw;
+        }
+
+        #${uid}.venmo-overlay-context-${CONTEXT.IFRAME} .venmo-checkout-iframe-container-full,
+        #${uid}.venmo-overlay-context-${CONTEXT.IFRAME} .venmo-checkout-iframe-container-full > .${CLASS.OUTLET},
+        #${uid}.venmo-overlay-context-${CONTEXT.IFRAME} .venmo-checkout-iframe-container-full > .${CLASS.OUTLET} > iframe {
+            height: 100vh;
+            max-width: 100vw;
+            width: 100vw;
+        }
+
+        @media screen and (max-width: 470px) {
+            #${uid}.venmo-overlay-context-${CONTEXT.IFRAME} .venmo-checkout-iframe-container,
+            #${uid}.venmo-overlay-context-${CONTEXT.IFRAME} .venmo-checkout-iframe-container > .${CLASS.OUTLET},
+            #${uid}.venmo-overlay-context-${CONTEXT.IFRAME} .venmo-checkout-iframe-container > .${CLASS.OUTLET} > iframe {
+                max-height: 85vh;
+            }
+            #${uid}.venmo-overlay-context-${CONTEXT.IFRAME} .venmo-checkout-iframe-container-full,
+            #${uid}.venmo-overlay-context-${CONTEXT.IFRAME} .venmo-checkout-iframe-container-full > .${CLASS.OUTLET},
+            #${uid}.venmo-overlay-context-${CONTEXT.IFRAME} .venmo-checkout-iframe-container-full > .${CLASS.OUTLET} > iframe {
+                height: 100vh;
+            }
+        }
+
+        #${uid}.venmo-overlay-context-${CONTEXT.IFRAME} .venmo-checkout-iframe-container {
+
+            display: block;
+
+            position: absolute;
+
+            top: 50%;
+            left: 50%;
+
+            min-width: 450px;
+
+            transform: translate(-50%, -50%);
+            transform: translate3d(-50%, -50%, 0);
+
+            border-radius: 10px;
+            overflow: hidden;
+        }
+
+        #${uid}.venmo-overlay-context-${CONTEXT.IFRAME} .${CLASS.OUTLET} {
+
+            position: relative;
+
+            transition: all 0.3s ease;
+            animation-duration: 0.3s;
+            animation-fill-mode: forwards !important;
+
+            min-width: 450px;
+            max-width: 450px;
+            width: 450px;
+            height: 535px;
+
+            background-color: white;
+
+            overflow: auto;
+
+            opacity: 0;
+            transform: scale3d(.3, .3, .3);
+
+            -webkit-overflow-scrolling: touch;
+        }
+
+        #${uid}.venmo-overlay-context-${CONTEXT.IFRAME} .${CLASS.OUTLET} > iframe {
+            position: absolute;
+            top: 0;
+            left: 0;
+            transition: opacity .4s ease-in-out;
+        }
+
+        #${uid}.venmo-overlay-context-${CONTEXT.IFRAME} .${CLASS.OUTLET} > iframe.${CLASS.COMPONENT_FRAME} {
+            z-index: 100;
+        }
+
+        #${uid}.venmo-overlay-context-${CONTEXT.IFRAME} .${CLASS.OUTLET} > iframe.${CLASS.PRERENDER_FRAME} {
+            z-index: 200;
+        }
+
+        #${uid}.venmo-overlay-context-${CONTEXT.IFRAME} .${CLASS.OUTLET} > iframe.${CLASS.VISIBLE} {
+            opacity: 1;
+            z-index: 200;
+        }
+
+        #${uid}.venmo-overlay-context-${CONTEXT.IFRAME} .${CLASS.OUTLET} > iframe.${CLASS.INVISIBLE} {
+            opacity: 0;
+            z-index: 100;
+        }
+
+        @media screen and (max-width: 470px) {
+
+            #${uid}.venmo-overlay-context-${CONTEXT.IFRAME} .venmo-checkout-iframe-container,
+            #${uid}.venmo-overlay-context-${CONTEXT.IFRAME} .${CLASS.OUTLET} {
+                min-width: 100%;
+                min-width: calc(100% - 20px);
+
+                max-width: 100%;
+                max-width: calc(100% - 20px);
+            }
+        }
+
+        #${uid}.venmo-overlay-context-${CONTEXT.IFRAME} .${CLASS.OUTLET} iframe {
+            width: 1px;
+            min-width: 100%;
+            height: 100%;
+        }
+
+        @keyframes show-component {
+            from {
+                opacity: 0;
+                transform: scale3d(.3, .3, .3);
+            }
+
+            to {
+                opacity: 1;
+                transform: scale3d(1, 1, 1);
+            }
+        }
+
+        @keyframes hide-component {
+            from {
+                opacity: 1;
+                transform: scale3d(1, 1, 1);
+            }
+
+            to {
+                opacity: 0;
+                transform: scale3d(.3, .3, .3);
+            }
+        }
+
+        .venmo-spinner {
+            height: 30px;
+            width: 30px;
+            display: inline-block;
+            box-sizing: content-box;
+            opacity: 1;
+            filter: alpha(opacity=100);
+            animation: rotation .7s infinite linear;
+            border-left: 8px solid rgba(0, 0, 0, .2);
+            border-right: 8px solid rgba(0, 0, 0, .2);
+            border-bottom: 8px solid rgba(0, 0, 0, .2);
+            border-top: 8px solid #fff;
+            border-radius: 100%
+        }
+
+        @keyframes rotation {
+            from {
+                transform: rotate(0deg)
+            }
+            to {
+                transform: rotate(359deg)
+            }
+        }
+    `;
+}
