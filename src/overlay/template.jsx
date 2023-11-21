64910253042bca8b07d0ab2e0af8b51d5ebef351
Sebastian Fernandez <sebfernandez@paypal.com>
2023-11-21 12:11:07 -0600

    fix: plan for ocasional window.alert async behavior and make sure focus is not triggered before the alert is acknowledged.

--- src/overlay/template.jsx
+++ src/overlay/template.jsx
@@ -15,10 +15,21 @@ import {
 } from "@krakenjs/belter/src";
 import { EVENT, CONTEXT } from "@krakenjs/zoid/src";
 import { node, type ElementNode } from "@krakenjs/jsx-pragmatic/src";
-import { LOGO_COLOR, PPLogo, PayPalLogo } from "@paypal/sdk-logos/src";
+import {
+  LOGO_COLOR,
+  PPLogo,
+  PayPalLogo,
+  VenmoLogo,
+} from "@paypal/sdk-logos/src";
 import type { ZalgoPromise } from "@krakenjs/zalgo-promise/src";
 
-import { getContainerStyle, getSandboxStyle, CLASS } from "./style";
+import {
+  getContainerStyle,
+  getSandboxStyle,
+  getVenmoContainerStyle,
+  getVenmoSandboxStyle,
+  CLASS,
+} from "./style";
 
 export type OverlayProps = {|
   context: $Values<typeof CONTEXT>,
@@ -30,6 +41,8 @@ export type OverlayProps = {|
   content?: void | {|
     windowMessage?: string,
     continueMessage?: string,
+    cancelMessage?: string,
+    interrogativeMessage?: string,
   |},
   autoResize?: boolean,
   hideCloseButton?: boolean,
@@ -52,6 +65,14 @@ export function Overlay({
 }: OverlayProps): ElementNode {
   const uid = `paypal-overlay-${uniqueID()}`;
 
+  function showAlert(message) {
+    return new Promise(function (resolve) {
+      // eslint-disable-next-line no-alert
+      window.alert(message);
+      resolve();
+    });
+  }
+
   function closeCheckout(e) {
     e.preventDefault();
     e.stopPropagation();
@@ -68,15 +89,16 @@ export function Overlay({
 
     // Note: alerts block the event loop until they are closed.
     if (isIos()) {
-      // eslint-disable-next-line no-alert
-      window.alert("Please switch tabs to reactivate the PayPal window");
-    } else if (isFirefox()) {
-      // eslint-disable-next-line no-alert
-      window.alert(
-        'Don\'t see the popup window after clicking "OK"?\n\nSelect "Window" in your toolbar to find "Log in to your PayPal account"'
+      showAlert("Please switch tabs to reactivate the PayPal window").then(() =>
+        focus()
       );
+    } else if (isFirefox()) {
+      showAlert(
+        'Don\'t see the popup window after clicking "OK"?\n\nSelect "Window" in your toolbar to find "Log in to your PayPal account"'
+      ).then(() => focus());
+    } else {
+      focus();
     }
-    focus();
   }
 
   const setupAnimations = (name) => {
@@ -202,6 +224,181 @@ export function Overlay({
               </div>
 
               <style nonce={nonce}>{getContainerStyle({ uid })}</style>
+            </div>
+          </body>
+        </html>
+      </iframe>
+    </div>
+  );
+}
+
+export function VenmoOverlay({
+  context,
+  close,
+  focus,
+  event,
+  frame,
+  prerenderFrame,
+  content = {},
+  autoResize,
+  hideCloseButton,
+  nonce,
+  fullScreen = false,
+}: OverlayProps): ElementNode {
+  const uid = `venmo-overlay-${uniqueID()}`;
+
+  function closeCheckout(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    close();
+  }
+
+  function focusCheckout(e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    if (!supportsPopups()) {
+      return;
+    }
+
+    // Note: alerts block the event loop until they are closed.
+    if (isIos()) {
+      // eslint-disable-next-line no-alert
+      window.alert("Please switch tabs to reactivate the Venmo window");
+    } else if (isFirefox()) {
+      // eslint-disable-next-line no-alert
+      window.alert(
+        'Don\'t see the popup window after clicking "OK"?\n\nSelect "Window" in your toolbar to find "Log in to your Venmo account"'
+      );
+    }
+    focus();
+  }
+
+  const setupAnimations = (name) => {
+    return (el) => {
+      const showContainer = () => animate(el, `show-${name}`, noop);
+      const hideContainer = () => animate(el, `hide-${name}`, noop);
+      event.on(EVENT.DISPLAY, showContainer);
+      event.on(EVENT.CLOSE, hideContainer);
+    };
+  };
+
+  const setupAutoResize = (el) => {
+    event.on(EVENT.RESIZE, ({ width: newWidth, height: newHeight }) => {
+      if (typeof newWidth === "number") {
+        el.style.width = toCSS(newWidth);
+      }
+
+      if (typeof newHeight === "number") {
+        el.style.height = toCSS(newHeight);
+      }
+    });
+  };
+
+  const outletOnRender = (el) => {
+    setupAnimations("component")(el);
+    if (autoResize) {
+      setupAutoResize(el);
+    }
+  };
+
+  let outlet;
+
+  if (frame && prerenderFrame) {
+    frame.classList.add(CLASS.COMPONENT_FRAME);
+    prerenderFrame.classList.add(CLASS.PRERENDER_FRAME);
+
+    prerenderFrame.classList.add(CLASS.VISIBLE);
+    frame.classList.add(CLASS.INVISIBLE);
+
+    event.on(EVENT.RENDERED, () => {
+      prerenderFrame.classList.remove(CLASS.VISIBLE);
+      prerenderFrame.classList.add(CLASS.INVISIBLE);
+
+      frame.classList.remove(CLASS.INVISIBLE);
+      frame.classList.add(CLASS.VISIBLE);
+
+      setTimeout(() => {
+        destroyElement(prerenderFrame);
+      }, 1);
+    });
+
+    outlet = (
+      <div class={CLASS.OUTLET} onRender={outletOnRender}>
+        <node el={frame} />
+        <node el={prerenderFrame} />
+      </div>
+    );
+  }
+
+  return (
+    <div
+      id={uid}
+      onRender={setupAnimations("container")}
+      class="venmo-checkout-sandbox"
+    >
+      <style nonce={nonce}>{getVenmoSandboxStyle({ uid })}</style>
+      <iframe
+        title="Venmo Checkout Overlay"
+        name={`__venmo_checkout_sandbox_${uid}__`}
+        scrolling="no"
+        class={`venmo-checkout-sandbox-iframe${fullScreen ? "-full" : ""}`}
+      >
+        <html>
+          <body>
+            <div
+              id={uid}
+              onClick={focusCheckout}
+              class={`venmo-overlay-context-${context} venmo-checkout-overlay`}
+            >
+              {!fullScreen && (
+                <div class="venmo-checkout-modal">
+                  <div class="venmo-checkout-logo">
+                    <VenmoLogo logoColor={LOGO_COLOR.WHITE} />
+                  </div>
+                  {content.interrogativeMessage && (
+                    <div class="venmo-interrogative-message">
+                      {content.interrogativeMessage}
+                    </div>
+                  )}
+                  {content.windowMessage && (
+                    <div class="venmo-checkout-message">
+                      {content.windowMessage}
+                    </div>
+                  )}
+                  {content.continueMessage && (
+                    <div class="venmo-checkout-continue">
+                      {/* This handler should be guarded with e.stopPropagation. 
+                          This will stop the event from bubbling up to the overlay click handler
+                          and causing unexpected behavior. */}
+                      <a onClick={focusCheckout} href="#">
+                        {content.continueMessage}
+                      </a>
+                    </div>
+                  )}
+                  {content.cancelMessage && !hideCloseButton && (
+                    <div class="venmo-checkout-close">
+                      <a href="#" onClick={closeCheckout} aria-label="close">
+                        {content.cancelMessage}
+                      </a>
+                    </div>
+                  )}
+                  <div class="venmo-checkout-loader">
+                    <div class="venmo-spinner" />
+                  </div>
+                </div>
+              )}
+              <div
+                class={
+                  fullScreen
+                    ? "venmo-checkout-iframe-container-full"
+                    : "venmo-checkout-iframe-container"
+                }
+              >
+                {outlet}
+              </div>
+
+              <style nonce={nonce}>{getVenmoContainerStyle({ uid })}</style>
             </div>
           </body>
         </html>
